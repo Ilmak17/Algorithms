1921ed2c415b446f4942e3d2722609ce8917cc1c
Ilyas Makirov <ilmak1704@gmail.com>
2024-10-28 22:59:17 +0500

    Add solution for LeetCode 19

--- src/linkedlist/README.md
+++ src/linkedlist/README.md
@@ -9,3 +9,8 @@
 
 - **Link to problem**: [LeetCode 206](https://leetcode.com/problems/linked-list-cycle/)
 - **Solution Code**: [LinkedListCycle_141.java](LinkedListCycle_141.java)
+
+### Problem: Remove Nth Node From End of List - LeetCode 19
+
+- **Link to problem**: [LeetCode 206](https://leetcode.com/problems/remove-nth-node-from-end-of-list/)
+- **Solution Code**: [RemoveNthNodeFromEndOfList_19.java](RemoveNthNodeFromEndOfList_19.java)
---
+++ src/linkedlist/RemoveNthNodeFromEndOfList_19.java
@@ -0,0 +1,34 @@
+package linkedlist;
+
+/*
+ * LeetCode Problem 19: Remove Nth Node From End of List
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ */
+
+
+public class RemoveNthNodeFromEndOfList_19 {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        int length = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
+            length++;
+        }
+
+        cur = dummyHead;
+        for (int i = 0; i < length - n; i++) {
+            cur = cur.next;
+        }
+
+        cur.next = cur.next.next;
+
+        return dummyHead.next;
+    }
+}
