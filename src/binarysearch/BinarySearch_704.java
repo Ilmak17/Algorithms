d63c4c870eb6d0a16f09c0a1ec06b00a63056956
Ilyas Makirov <ilmak1704@gmail.com>
2024-10-29 22:04:29 +0500

    Add solution for LeetCode 704

---
+++ src/binarysearch/BinarySearch_704.java
@@ -0,0 +1,31 @@
+package binarysearch;
+
+/*
+ * LeetCode Problem 704: Binary Search
+ * Difficulty: Easy
+ *
+ * Problem Statement:
+ * Binary Search
+ */
+
+public class BinarySearch_704 {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            } else if(nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ src/binarysearch/README.md
@@ -0,0 +1,8 @@
+## Binary Search
+
+### Problem: Binary Search  - LeetCode 704
+
+- **Link to problem**: [LeetCode 704](https://leetcode.com/problems/binary-search/)
+- **Solution Code**: [BinarySearch_704.java](BinarySearch_704.java)
+
+
