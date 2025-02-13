9e10c3d02fedb0ffff1b1be105b20646eec0db11
Ilyas Makirov <ilmak1704@gmail.com>
2025-02-13 23:59:33 +0500

    Add solution for LeetCode 3066

---
+++ src/arrayshashing/MinimumOperationsExceedThresholdValueII_3066.java
@@ -0,0 +1,39 @@
+package arrayshashing;
+
+/*
+ * LeetCode Problem 238: Product of Array Except Self
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements
+ * of nums except nums[i].
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ */
+
+import java.util.PriorityQueue;
+
+public class MinimumOperationsExceedThresholdValueII_3066 {
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            if (num < k) pq.add(num);
+        }
+
+        int operations = 0;
+
+        while (!pq.isEmpty()) {
+            int x = pq.poll();
+            operations++;
+
+            if (pq.isEmpty()) break;
+
+            int y = pq.poll();
+            long newValue = 2L * x + y;
+
+            if (newValue < k) pq.add((int) newValue);
+        }
+
+        return operations;
+    }
+}
