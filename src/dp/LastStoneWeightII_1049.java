33301703d7f398ab6fc17b180c80e3d9f12c7dc1
Ilyas Makirov <ilmak1704@gmail.com>
2025-07-02 21:25:48 +0500

    Add solution for LeetCode 1049

--- src/dp/LastStoneWeightII_1049.java
+++ src/dp/LastStoneWeightII_1049.java
@@ -9,5 +9,31 @@ package dp;
  */
 
 public class LastStoneWeightII_1049 {
+    public int lastStoneWeightII(int[] stones) {
+        int total = 0;
+        for (int stone : stones) {
+            total += stone;
+        }
+        int target = total / 2;
 
+        Integer[][] memo = new Integer[stones.length][target + 1];
+        int best = dfs(stones, 0, target, memo);
+
+        return total - 2 * best; // total - 2 * s1 = |s1 - s2|
+    }
+
+    private int dfs(int[] stones, int i, int remaining, Integer[][] memo) {
+        if (i == stones.length) return 0;
+        if (memo[i][remaining] != null) return memo[i][remaining];
+
+        int without = dfs(stones, i + 1, remaining, memo);
+
+        int with = 0;
+        if (stones[i] <= remaining) {
+            with = stones[i] + dfs(stones, i + 1, remaining - stones[i], memo);
+        }
+
+        memo[i][remaining] = Math.max(with, without);
+        return memo[i][remaining];
+    }
 }
