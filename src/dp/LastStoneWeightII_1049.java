1256de532ffb866779853bb6b67b3c401f1be268
Ilyas Makirov <ilmak1704@gmail.com>
2025-07-05 20:37:39 +0500

    Add solution for LeetCode 1049 bottom up approach

--- src/dp/LastStoneWeightII_1049.java
+++ src/dp/LastStoneWeightII_1049.java
@@ -36,4 +36,27 @@ public class LastStoneWeightII_1049 {
         memo[i][remaining] = Math.max(with, without);
         return memo[i][remaining];
     }
+
+    public int lastStoneWeightIIBottomUp(int[] stones) {
+        int stoneSum = 0;
+        for (int stone : stones) {
+            stoneSum += stone;
+        }
+        int target = stoneSum / 2;
+        int n = stones.length;
+
+        int[][] dp = new int[n + 1][target + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int t = 0; t <= target; t++) {
+                if (t >= stones[i - 1]) {
+                    dp[i][t] = Math.max(dp[i - 1][t], dp[i - 1][t - stones[i - 1]] + stones[i - 1]);
+                } else {
+                    dp[i][t] = dp[i - 1][t];
+                }
+            }
+        }
+
+        return stoneSum - 2 * dp[n][target];
+    }
 }
