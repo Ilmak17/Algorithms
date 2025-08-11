45289e656d87f5e61b61e51cba4344a29f791638
Ilyas Makirov <ilmak1704@gmail.com>
2025-08-11 23:44:55 +0500

    Add solution for LeetCode 611

---
+++ src/binarysearch/ValidTriangleNumber_611.java
@@ -0,0 +1,15 @@
+package binarysearch;
+
+/*
+ * LeetCode Problem 611:
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+ *
+ */
+
+import java.util.Arrays;
+
+public class ValidTriangleNumber_611 {
+
+}
