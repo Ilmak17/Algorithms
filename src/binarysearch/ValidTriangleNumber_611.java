549703bc798c5ce973e2a31eb29cd19d19c4d20c
Ilyas Makirov <ilmak1704@gmail.com>
2025-08-13 23:59:21 +0500

    Add solution for LeetCode 611

--- src/binarysearch/ValidTriangleNumber_611.java
+++ src/binarysearch/ValidTriangleNumber_611.java
@@ -15,7 +15,6 @@ public class ValidTriangleNumber_611 {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length, count = 0;
-
         for (int k = n - 1; k >= 2; k--) {
             int i = 0, j = k - 1;
             while (i < j) {
