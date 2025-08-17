3fd0f8f8524699dce17e242504300f8f3f629a8b
Ilyas Makirov <ilmak1704@gmail.com>
2025-08-17 19:50:09 +0500

    Add solution for LeetCode 1802

---
+++ src/binarysearch/MaximumValueGivenIndexBoundedArray_1802.java
@@ -0,0 +1,61 @@
+package binarysearch;
+
+/*
+ * LeetCode Problem 1802: Maximum Value at a Given Index in a Bounded Array
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+ * You are given three positive integers: n, index, and maxSum.
+ * You want to construct an array nums (0-indexed) that satisfies the following conditions:
+ * nums.length == n
+ * nums[i] is a positive integer where 0 <= i < n.
+ * abs(nums[i] - nums[i+1]) <= 1 where 0 <= i < n-1.
+ * The sum of all the elements of nums does not exceed maxSum.
+ * nums[index] is maximized.
+ * Return nums[index] of the constructed array.
+ * Note that abs(x) equals x if x >= 0, and -x otherwise.
+ */
+
+public class MaximumValueGivenIndexBoundedArray_1802 {
+
+    private long getSum(int index, int value, int n) {
+        long count = 0;
+
+        // On index's left:
+        // If value > index, there are index + 1 numbers in the arithmetic sequence:
+        // [value - index, ..., value - 1, value].
+        // Otherwise, there are value numbers in the arithmetic sequence:
+        // [1, 2, ..., value - 1, value], plus a sequence of length (index - value + 1) of 1s.
+        if (value > index) {
+            count += (long)(value + value - index) * (index + 1) / 2;
+        } else {
+            count += (long)(value + 1) * value / 2 + index - value + 1;
+        };
+        // On index's right:
+        // If value >= n - index, there are n - index numbers in the arithmetic sequence:
+        // [value, value - 1, ..., value - n + 1 + index].
+        // Otherwise, there are value numbers in the arithmetic sequence:
+        // [value, value - 1, ..., 1], plus a sequence of length (n - index - value) of 1s.
+        if (value >= n - index) {
+            count += (long)(value + value - n + 1 + index) * (n - index) / 2;
+        } else {
+            count += (long)(value + 1) * value / 2 + n - index - value;
+        }
+
+        return count - value;
+
+    }
+    public int maxValue(int n, int index, int maxSum) {
+        int left = 1, right = maxSum;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (getSum(index, mid, n) <= maxSum) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
