e061281a45ee1769cd30343ece3544f58e5c271c
Ilyas Makirov <ilmak1704@gmail.com>
2025-08-21 17:10:14 +0500

    Add solution for LeetCode 1235

---
+++ src/binarysearch/MaximumProfitJobScheduling_1235.java
@@ -0,0 +1,13 @@
+package binarysearch;
+
+/*
+ * LeetCode Problem 1235: Russian Doll Envelopes
+ * Difficulty: Hard
+ *
+ * Problem Statement:
+ *
+ */
+
+
+public class MaximumProfitJobScheduling_1235 {
+}
