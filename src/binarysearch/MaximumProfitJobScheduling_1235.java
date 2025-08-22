80d7ef35db71c70674243ad8ec921ebfee5f60c1
Ilyas Makirov <ilmak1704@gmail.com>
2025-08-22 09:00:14 +0500

    Add solution for LeetCode 1235

--- src/binarysearch/MaximumProfitJobScheduling_1235.java
+++ src/binarysearch/MaximumProfitJobScheduling_1235.java
@@ -1,13 +1,71 @@
 package binarysearch;
 
 /*
- * LeetCode Problem 1235: Russian Doll Envelopes
+ * LeetCode Problem 1235: Maximum Profit in Job Scheduling
  * Difficulty: Hard
  *
  * Problem Statement:
- *
+ * We have n jobs, where every job is scheduled to be done from startTime[i] to
+ * endTime[i], obtaining a profit of profit[i].
+ * You're given the startTime, endTime and profit arrays, return the maximum profit
+ * you can take such that there are no two jobs in the subset with overlapping time range.
+ * If you choose a job that ends at time X you will be able to start another job that starts at time X.
  */
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class MaximumProfitJobScheduling_1235 {
+    class The_Comparator implements Comparator<ArrayList<Integer>> {
+        public int compare(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+            return list1.get(0) - list2.get(0);
+        }
+    }
+
+    private int findMaxProfit(List<List<Integer>> jobs) {
+        int n = jobs.size(), maxProfit = 0;
+        PriorityQueue<ArrayList<Integer>> pq = new PriorityQueue<>(new The_Comparator());
+
+        for (int i = 0; i < n; i++) {
+            int start = jobs.get(i).get(0), end = jobs.get(i).get(1), profit = jobs.get(i).get(2);
+
+            while (pq.isEmpty() == false && start >= pq.peek().get(0)) {
+                maxProfit = Math.max(maxProfit, pq.peek().get(1));
+                pq.remove();
+            }
+
+            ArrayList<Integer> combinedJob = new ArrayList<>();
+            combinedJob.add(end);
+            combinedJob.add(profit + maxProfit);
+
+            pq.add(combinedJob);
+        }
+
+        while (pq.isEmpty() == false) {
+            maxProfit = Math.max(maxProfit, pq.peek().get(1));
+            pq.remove();
+        }
+
+        return maxProfit;
+    }
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        List<List<Integer>> jobs = new ArrayList<>();
+
+        int length = profit.length;
+        for (int i = 0; i < length; i++) {
+            ArrayList<Integer> currJob = new ArrayList<>();
+            currJob.add(startTime[i]);
+            currJob.add(endTime[i]);
+            currJob.add(profit[i]);
+
+            jobs.add(currJob);
+        }
+
+        jobs.sort(Comparator.comparingInt(a -> a.get(0)));
+        return findMaxProfit(jobs);
+    }
 }
