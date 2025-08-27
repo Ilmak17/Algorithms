39ec44469300b5406666f58fa0a8a6ab9e051fda
Ilyas Makirov <ilmak1704@gmail.com>
2025-08-27 20:34:49 +0500

    Add solution for LeetCode 841

---
+++ src/graph/KeysAndRooms_841.java
@@ -0,0 +1,35 @@
+package graph;
+
+/*
+ * LeetCode Problem 841: Keys and Rooms
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+ * There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to
+ * visit all the rooms. However, you cannot enter a locked room without having its key.
+ * When you visit a room, you may find a set of distinct keys in it. Each key has a number on it,
+ * denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+ * Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i,
+ *  return true if you can visit all the rooms, or false otherwise.
+ */
+
+import java.util.List;
+
+public class KeysAndRooms_841 {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        dfs(0, rooms, visited);
+
+        for (boolean v : visited) if (!v) return false;
+        return true;
+    }
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
+        if (visited[room]) return;
+        visited[room] = true;
+        for (int key : rooms.get(room)) {
+            if (!visited[key]) dfs(key, rooms, visited);
+        }
+    }
+}
