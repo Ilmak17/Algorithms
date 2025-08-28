d7c9c40686b3f5140a32e6cec19d843a6a03f244
Ilyas Makirov <ilmak1704@gmail.com>
2025-08-28 23:47:18 +0500

    Add solution for LeetCode 1129

---
+++ src/graph/ShortestPathAlternatingColors_1129.java
@@ -0,0 +1,15 @@
+package graph;
+
+/*
+ * LeetCode Problem 841: Keys and Rooms
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+
+ */
+
+import java.util.List;
+
+public class ShortestPathAlternatingColors_1129 {
+
+}
