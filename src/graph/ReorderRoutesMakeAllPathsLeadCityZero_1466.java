e14332f51642a91b89ccf5e9f9b70671911dcfb5
Ilyas Makirov <ilmak1704@gmail.com>
2025-09-01 23:48:21 +0500

    Add solution for LeetCode 1466

--- src/graph/ReorderRoutesMakeAllPathsLeadCityZero_1466.java
+++ src/graph/ReorderRoutesMakeAllPathsLeadCityZero_1466.java
@@ -14,13 +14,6 @@ package graph;
  * It's guaranteed that each city can reach city 0 after reorder.
  */
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 
 public class ReorderRoutesMakeAllPathsLeadCityZero_1466 {
     public int minReorder(int n, int[][] connections) {
