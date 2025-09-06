925fbfb0a4c99a5edf881cada7fa7d42ff5671f0
Ilyas Makirov <ilmak1704@gmail.com>
2025-09-06 19:01:19 +0500

    Add solution for LeetCode 542

---
+++ src/graph/Matrix01_542.java
@@ -0,0 +1,57 @@
+package graph;
+
+/*
+ * LeetCode Problem 542: 01 Matrix
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+ * Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+ * The distance between two cells sharing a common edge is 1.
+ */
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class Matrix01_542 {
+    public int[][] updateMatrix(int[][] mat) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        int[][] dist = new int[rows][cols];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[] { i, j });
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int row = cur[0];
+            int col = cur[1];
+
+            for (int[] dir : dirs) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+                if (Math.min(newRow, newCol) < 0 || newRow >= rows || newCol >= cols)
+                    continue;
+                if (dist[newRow][newCol] > dist[row][col] + 1) {
+                    dist[newRow][newCol] = dist[row][col] + 1;
+                    queue.add(new int[] { newRow, newCol });
+                }
+            }
+        }
+
+        return dist;
+    }
+}
